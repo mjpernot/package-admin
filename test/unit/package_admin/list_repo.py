41d212d8b9475d128840dc1291522bd28c2aa965
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-09-26 10:20:34 +0000

    Update list_repo.py

--- test/unit/package_admin/list_repo.py
+++ test/unit/package_admin/list_repo.py
@@ -109,7 +109,6 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        # Set mock values.
         mock_yum.return_value = True
 
         self.assertFalse(package_admin.list_repo(self.args_array, self.yum))
