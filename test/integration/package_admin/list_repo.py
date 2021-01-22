43137cf69868f46aaa4ee832acaa755ccc8bda8d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-01-22 13:39:57 +0000

    Updated test/integration/package_admin/list_repo.py v2.3.0

--- test/integration/package_admin/list_repo.py
+++ test/integration/package_admin/list_repo.py
@@ -32,7 +32,6 @@ import mock
 sys.path.append(os.getcwd())
 import package_admin
 import lib.gen_libs as gen_libs
-import lib.cmds_gen as cmds_gen
 import mongo_lib.mongo_libs as mongo_libs
 import mongo_lib.mongo_class as mongo_class
 import version
@@ -254,7 +253,7 @@ class UnitTest(unittest.TestCase):
             True if mongo.coll_find1()["Server"] == self.yum.hostname \
             else False
 
-        cmds_gen.disconnect([mongo])
+        mongo_libs.disconnect([mongo])
 
         self.assertTrue(status)
 
@@ -284,7 +283,7 @@ class UnitTest(unittest.TestCase):
         else:
             status = False
 
-        cmds_gen.disconnect([mongo])
+        mongo_libs.disconnect([mongo])
 
         self.assertTrue(status)
 
@@ -305,7 +304,7 @@ class UnitTest(unittest.TestCase):
 
         mongo.db_connect(self.dbn)
         mongo.db_cmd("dropDatabase")
-        cmds_gen.disconnect([mongo])
+        mongo_libs.disconnect([mongo])
 
         if os.path.isfile(self.out_file):
             os.remove(self.out_file)
